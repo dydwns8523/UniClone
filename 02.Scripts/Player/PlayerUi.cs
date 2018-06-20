7863099454c69e8dc0ca9d9869d3a9a502961a42
dydwns8523 <40413894+dydwns8523@users.noreply.github.com>
2018-06-20 16:06:06 +0900

    Add files via upload

---
+++ 02.Scripts/Player/PlayerUi.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUi : MonoBehaviour {
+	[SerializeField]
+	private GameObject Energy;
+
+    [SerializeField]
+    private GameObject Health;
+
+    [SerializeField]
+    private GameObject winnerScreen;
+
+    public GameObject backCamMask;
+
+	public GameObject miniCamMask;
+
+    public GameObject aimCircle;
+
+    public GameObject[] invenBG;
+
+    public GameObject[] Guns;
+
+    [SerializeField]
+    private PlayerStats playerStats;
+
+    Image EnergyImage;
+    Image HealthImage;
+
+    void Update()
+    {
+        if (GameDirector.Instance.GetComponent<GameStart>().GetStart)
+        {
+            if (GameDirector.Instance.players.Count == 1 && !GameDirector.Instance.isPlayerDead)
+            {
+                winnerScreen.SetActive(true);
+            }
+        } 
+    }
+
+    private void Start()
+	{
+		EnergyImage = Energy.GetComponent<Image>();
+        HealthImage = Health.GetComponent<Image>();
+    }
+
+	public void SetEnergy(float e)
+    {
+        EnergyImage.fillAmount = e / playerStats.EnergyMax;
+    }
+
+    public void SetHealth(float h)
+    {
+        HealthImage.fillAmount = h / playerStats.HealthMax;        
+    }
+
+    public void SetEnergyBar(float e)
+    {
+        EnergyImage.fillAmount = e / playerStats.EnergyMax;
+    }
+
+    public void SetHealthBar(float h)
+    {
+        HealthImage.fillAmount = h / playerStats.HealthMax;
+    }
+}
